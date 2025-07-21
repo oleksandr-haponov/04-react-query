c2fd90ce02878bdae552157c22ee76fb72b86c4d
Oleksandr Haponov <olex.haponov@gmail.com>
2025-07-21 22:38:35 +0300

    Update movieService.ts

--- src/services/movieService.ts
+++ src/services/movieService.ts
@@ -1,15 +1,22 @@
-import axios from 'axios'
-import type { Movie } from '../types/movie'
+import axios from 'axios';
+import type { Movie } from '../types/movie';
 
-const BASE_URL = 'https://api.themoviedb.org/3/search/movie'
+const BASE_URL = 'https://api.themoviedb.org/3/search/movie';
+
+interface TMDBResponse {
+  results: Movie[];
+  total_pages: number;
+  total_results: number;
+  page: number;
+}
 
 export async function fetchMovies(query: string): Promise<Movie[]> {
-  const response = await axios.get(BASE_URL, {
+  const response = await axios.get<TMDBResponse>(BASE_URL, {
     params: { query },
     headers: {
       Authorization: `Bearer ${import.meta.env.VITE_TMDB_TOKEN}`,
     },
-  })
+  });
 
-  return response.data.results
+  return response.data.results;
 }
